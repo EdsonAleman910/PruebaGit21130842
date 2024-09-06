8fe9beb401943fc3fbba44f1edad0a18c6ae5083
EdsonAleman910 <edsnalemancu@gmail.com>
2024-09-06 13:42:11 -0600

    Proyecto Inicial

--- app/src/main/java/com/example/pruebagit21130842/MainActivity.kt
+++ app/src/main/java/com/example/pruebagit21130842/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PruebaGit21130842Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android", //Master
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
